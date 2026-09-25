35123a88d46fe5d723fcb9a1b8a68f585aa06f36
ADC508 <alphadentalclinic786@gmail.com>
2026-09-26 02:44:54 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='adc-mobile-v1-step1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match('./index.html')))));
